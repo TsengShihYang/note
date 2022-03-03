ab994827da051c89f5df9ae587f077e39386eb5f
TSY <iamdog43@gmail.com>
2022-03-04 01:43:57 +0800

    增加後臺文章管理

--- w2_3.php
+++ w2_3.php
@@ -26,7 +26,7 @@ location.reload();
 
 //匯入8篇文章入資料庫  寫分頁程式 並撈出全部文章 
 <?php
-$total=$News->math('count','*');
+$total=$News->math('count','*',['sh'=>1]);
 $div=5;
 $pages=ceil($total/$div);
 $now=$_GET['p']??1;
@@ -75,4 +75,55 @@ $(".switch").on("click",function(){
     $(this).parent().find(".short,.full").toggle()
 })
 </script>
-
+// news 第三行 td 放 按讚顯示
+<?php
+if(isset($_SESSION['login'])){
+    echo "<a class='g' data-type='2'>讚</a>";
+}
+?>
+//script 註冊一個按讚function 
+<script>
+$(".g").on("click",function(){
+        let type=$(this).data('type')
+        switch(type){
+            case 1:
+                $(this).text("讚")
+                $(this).data('type',2)
+                break;
+                case 2:
+                $(this).text("收回讚")
+                $(this).data('type',1)
+                break;
+        }
+})</script>
+//後臺文章管理  將前台的 news檔案拷過來
+//多設一個 $chk 與 hidden id 
+<?php
+$news=$News->all("limit $start,$div");
+foreach($news as $key => $row){
+    $chk=($row['sh']==1)?"checked":"";
+?>
+    <tr>
+        <td class="clo"><?=$start+1+$key;?></td>
+        <td class="clo"><?=$row['title'];?></td>
+        <td>
+    <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=$chk;?>>
+        </td>        
+        <td>
+    <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+    <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+        </td>
+    </tr>
+<?php
+}
+?>
+//api 新增 news_admin.php
+<?php include_once "../base.php";
+foreach($_POST['id'] as $id){
+    if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+    $News->del($id);
+}else{
+    $news=$News->find($id);
+    $news['sh']=(isset($_POST['del']) && in_array($id,$_POST['del']))?1:0;
+    $News->save($news);
+} }
