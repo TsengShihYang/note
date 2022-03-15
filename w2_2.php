249ebca9fafb482440f0b310c69302726f1296fe
TSY <iamdog43@gmail.com>
2022-03-15 17:22:18 +0800

    新增後臺程式

--- w2_2.php
+++ w2_2.php
@@ -48,3 +48,81 @@ $news=$News->find($id);
 //new line to br
 echo nl2br($news['text']); ?>
 
+//後臺帳號管理 form+table
+<?php
+    $user=$User->all();
+    foreach($users as $key=>$user){
+    ?>
+        <tr>
+            <td><?=$user['acc'];?></td>
+            <td><?=str_repeat("*",mb_strlen($user['pw']));?></td>
+            <td><input type="checkbox" name="del[]" value="<?=$user['id'];?>"></td>
+        </tr>
+    <?php
+    }
+    ?>
+//至api 寫 del.php
+<?php include_once "../base.php";
+if(isset($_POST['del'])){
+    foreach($_POST['del'] as $id)
+    $User->del($id);
+} 
+to("../back.php?do=admin"); ?>
+
+<?php
+//自front reg拷貝"會員註冊"(含script) 改成"新增會員"
+//改h1 刪多餘的</fieldset>
+//("註冊完成,歡迎加入") 後 location.reload();
+?>
+//匯入8篇文章入資料庫  寫分頁程式 並撈出全部文章 
+<?php
+$total=$News->math('count','*',['sh'=>1]);
+$div=5;
+$pages=ceil($total/$div);
+$now=$_GET['p']??1;
+$start=($now-1)*$div;
+
+$news=$News->all(['sh'=>1],"limit $start,$div");
+foreach($news as $key => $row){
+?>
+    <tr>
+        <td class="clo"><?=$row['title'] ;?></td>
+        <td class="switch">
+        <div class="short"><?=mb_substr($row['text'],0,20) ;?>...</div>
+        <div class="full" style="display: none;"><?=nl2br($row['text']) ;?></div>
+        </td>
+        <td></td>
+    </tr>
+<?php
+}
+?>
+</table>
+//在此寫分頁 並放大當前頁數字
+<div> <?php
+if(($now-1)>0){
+    $prev=$now-1;
+    echo "<a href='index.php?do=news&p=$prev'>";
+    echo " < ";
+    echo " </a>";
+}
+for($i=1;$i<=$pages;$i++){
+    $font=($now==$i)?'24px':'16px';
+    echo "<a href='index.php?do=news&p=$i' style='font-size:$font '>";
+    echo $i;
+    echo "</a>";
+}
+if(($now+1)<=$pages){
+    $next=$now+1;
+    echo "<a href='index.php?do=news&p=$next'>";
+    echo " > ";
+    echo " </a>";
+}
+?></div>
+</fieldset>
+//切換顯示文章 短版長版
+<script>
+$(".switch").on("click",function(){
+    $(this).parent().find(".short,.full").toggle()
+})
+</script>
+
--- w2_3.php
+++ w2_3.php
@@ -1,80 +1,3 @@
-//後臺帳號管理 form+table
-<?php
-    $user=$User->all();
-    foreach($users as $key=>$user){
-    ?>
-        <tr>
-            <td><?=$user['acc'];?></td>
-            <td><?=str_repeat("*",mb_strlen($user['pw']));?></td>
-            <td><input type="checkbox" name="del[]" value="<?=$user['id'];?>"></td>
-        </tr>
-    <?php
-    }
-    ?>
-//至api 寫 del.php
-<?php include_once "../base.php";
-if(isset($_POST['del'])){
-    foreach($_POST['del'] as $id)
-    $User->del($id);
-} 
-to("../back.php?do=admin"); ?>
-
-//自front reg拷貝"會員註冊"(含script) 改成"新增會員"
-改h1 刪多餘的</fieldset>
-("註冊完成,歡迎加入") 後 
-location.reload();
-
-//匯入8篇文章入資料庫  寫分頁程式 並撈出全部文章 
-<?php
-$total=$News->math('count','*',['sh'=>1]);
-$div=5;
-$pages=ceil($total/$div);
-$now=$_GET['p']??1;
-$start=($now-1)*$div;
-
-$news=$News->all(['sh'=>1],"limit $start,$div");
-foreach($news as $key => $row){
-?>
-    <tr>
-        <td class="clo"><?=$row['title'] ;?></td>
-        <td class="switch">
-        <div class="short"><?=mb_substr($row['text'],0,20) ;?>...</div>
-        <div class="full" style="display: none;"><?=nl2br($row['text']) ;?></div>
-        </td>
-        <td></td>
-    </tr>
-<?php
-}
-?>
-</table>
-//在此寫分頁 並放大當前頁數字
-<div> <?php
-if(($now-1)>0){
-    $prev=$now-1;
-    echo "<a href='index.php?do=news&p=$prev'>";
-    echo " < ";
-    echo " </a>";
-}
-for($i=1;$i<=$pages;$i++){
-    $font=($now==$i)?'24px':'16px';
-    echo "<a href='index.php?do=news&p=$i' style='font-size:$font '>";
-    echo $i;
-    echo "</a>";
-}
-if(($now+1)<=$pages){
-    $next=$now+1;
-    echo "<a href='index.php?do=news&p=$next'>";
-    echo " > ";
-    echo " </a>";
-}
-?></div>
-</fieldset>
-//切換顯示文章 短版長版
-<script>
-$(".switch").on("click",function(){
-    $(this).parent().find(".short,.full").toggle()
-})
-</script>
 // news 第三行 td 放 按讚顯示
 <?php
 if(isset($_SESSION['login'])){
@@ -126,4 +49,44 @@ foreach($_POST['id'] as $id){
     $news=$News->find($id);
     $news['sh']=(isset($_POST['del']) && in_array($id,$_POST['del']))?1:0;
     $News->save($news);
-} }
+} }  ?>
+//問卷選項 
+
+<form action="api/que.php" method="post">
+    <div  style="display: flex;">
+        <div class="clo">問卷名稱</div>
+        <div>
+        <input type="text" name="subject" >
+        </div>
+    </div>
+    <div class="clo" id="opt"> 
+        <div>
+     <span>選項</span><input type="text" name="options[]" >
+     <input type="button" onclick="more()" value="更多">
+        </div>
+    </div>
+    <div>
+        <input type="submit" value="新增"> | 
+        <input type="reset" value="清空">
+    </div>
+</form>
+</fieldset>
+<script>
+    function more(){
+        let opt=`
+        <div><span>選項</span><input type="text" name="options[]"></div>
+        `
+    $("#opt").prepend(opt);
+    }
+</script>
+//新增 api que.php
+<?php
+$subject=$_POST['subject'];
+$Que->save(['text'=>$subject,'parent'=> 0 , 'count'=> 0]);
+$parent_id=$Que->math('max','id');
+
+foreach($_POST['options'] as $opt){
+    $Que->save(['text'=>$opt,'parent'=>$parent_id,'count'=> 0]);
+}
+to("../back.php?do=que");  ?>
+
