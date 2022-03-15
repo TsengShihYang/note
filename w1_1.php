b3e9ff7cdb3b0ea19b9fec011252d2fa16678587
TSY <iamdog43@gmail.com>
2022-03-16 01:42:18 +0800

    新增w1 api程式

--- w1_1.php
+++ w1_1.php
@@ -35,12 +35,114 @@ if(!isset($_SESSION['total'])){
 <?=$Total->find(1)['total'];?> 
 <?php //api
 $Total->save(['id'=>1,'total'=>$_POST['total']]);
-to("../back.php?do=total"); //注意是路徑兩點
+to("../back.php?do=total");   ?>//注意是路徑兩點
 
 //完成頁尾版權管理 與相關api 注意input類型改text 
 <?=$Bottom->find(1)['bottom'];?> 
 <?php //api
 $Total->save(['id'=>1,'total'=>$_POST['total']]);
-to("../back.php?do=total");
+to("../back.php?do=total");    ?>
 
 //建立後台 title.php 為基礎 之後再複製至其他
+
+//api 程式 add.php
+<?php include  "../base.php";
+
+if(!empty($_FILES['img']['tmp_name'])){
+    move_uploaded_file($_FILES['img']['tmp_name'],"../img/".$_FILES['img']['name']);
+    $data['img']=$_FILES['img']['name'];
+}else{
+    if($DB->table!='admin' && $DB->table!='menu'){
+        $data['img']='';
+    }
+}
+switch($DB->table){
+    case "title":
+        $data['text']=$_POST['text'];
+        $data['sh']=0;
+    break;
+    case "admin":
+        $data['acc']=$_POST['acc'];
+        $data['pw']=$_POST['pw'];
+    break;
+    case "menu":
+        $data['name']=$_POST['name'];
+        $data['href']=$_POST['href'];
+        $data['sh']=1;
+        $data['parent']=0;
+    break;
+    default:
+        $data['text']=$_POST['text']??'';
+        $data['sh']=1;
+    break;
+}
+$DB->save($data);
+to("../back.php?do=".$DB->table)
+?>
+// api edit.php
+<?php include_once "../base.php";
+foreach($_POST['id'] as $key => $id){
+    if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+        $DB->del($id);
+    }else{
+        $data=$DB->find($id);
+        switch($DB->table){
+            case "title":
+                $data['text']=$_POST['text'][$key];
+                $data['sh']=($_POST['sh']==$id)?1:0;
+            break;
+            case "admin":
+                $data['acc']=$_POST['acc'][$key];
+                $data['pw']=$_POST['pw'][$key];
+            break;
+            case "menu":
+                $data['name']=$_POST['name'][$key];
+                $data['href']=$_POST['href'][$key];
+                $data['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+            break;
+            default:   //ad,,news,image,mvim
+                $data['text']=isset($_POST['text'])?$_POST['text'][$key]:'';
+                $data['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
+            break;
+        }
+        $DB->save($data);
+    }
+}
+to("../back.php?do=".$DB->table);   ?>
+
+// api upload.php  add拷貝過來改
+<?php include_once "../base.php";
+
+if(!empty($_FILES['img']['tmp_name'])){
+    move_uploaded_file($_FILES['img']['tmp_name'],"../img/".$_FILES['img']['name']);
+    $data=$DB->find($_POST['id']);
+    $data['img']=$_FILES['img']['name'];
+    $DB->save($data);
+}
+
+to("../back.php?do=".$DB->table);   ?>
+
+//api submenu.php
+<?php  include_once "../base.php";
+
+if(isset($_POST['id'])){
+    foreach($_POST['id'] as $key=>$id){
+        if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+            $Menu->del($id);
+        }else{
+            $sub=$Menu->find($id);
+            $sub['name']=$_POST['name'][$key];
+            $sub['href']=$_POST['href'][$key];
+            $Menu->save($sub);
+        }     }     }
+
+if(isset($_POST['name2'])){
+    foreach($_POST['name2'] as $key=>$name){
+        if($name!=''){
+            $Menu->save(['name'=>$name,
+                         'href'=>$_POST['href2'][$key],
+                         'sh'=>1,
+                         'parent'=>$_GET['main']]);
+        }      }     }
+
+to("../back.php?do=".$Menu->table); ?>
